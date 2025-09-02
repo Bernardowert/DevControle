afeabb2363db3dab6e414805de8caa313b815cac
devBernardo <bernardowerty@outlook.com>
2025-09-02 12:00:32 -0300

    criação do containerGRID, criação do componente header e instalação da lib TailwindMerge

---
+++ src/components/header/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { FiUser, FiLogOut } from "react-icons/fi";
+import { ContainerGRID } from "../containerGRID";
+
+export function Header(){
+    return(
+        <header className="flex items-center bg-white h-20 shadow-sm">
+           <ContainerGRID>
+                <Link href="/" className="font-bold text-2xl uppercase transition-all ease-linear [&:hover,&:focus]:tracking-widest">
+                  <span className="text-blue-500">Dev </span>
+                    Controle
+                </Link>
+                  <div className="flex items-center gap-4">
+                    <Link href="/dashboard" aria-label="ir para pagina dashboard">
+                        <FiUser size={26} color="#4b5563" />
+                    </Link>
+                    <button aria-label="botão para deslogar da conta">
+                        <FiLogOut size={26} color="#4b5563" />
+                    </button>
+                 </div>
+           </ContainerGRID>
+        </header>
+    )
+}
